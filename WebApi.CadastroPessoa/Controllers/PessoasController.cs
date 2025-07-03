6872e9ca2e9abc7a5df741f817beef5d5063f6f4
WaineAlvesCarneiro <carneirowaine@gmail.com>
2025-07-03 15:36:49 -0300

    Adicionar arquivos de projeto.

---
+++ WebApi.CadastroPessoa/Controllers/PessoasController.cs
@@ -0,0 +1,96 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using WebApi.CadastroPessoa.Data;
+using WebApi.CadastroPessoa.Models;
+
+namespace WebApi.CadastroPessoa.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class PessoasController : ControllerBase
+    {
+        private readonly WebApiCadastroPessoaContext _context;
+
+        public PessoasController(WebApiCadastroPessoaContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<Pessoa>>> GetPessoa()
+        {
+            return await _context.Pessoa.ToListAsync();
+        }
+
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Pessoa>> GetPessoa(int id)
+        {
+            var pessoa = await _context.Pessoa.FindAsync(id);
+
+            if (pessoa == null)
+            {
+                return NotFound();
+            }
+
+            return pessoa;
+        }
+
+        [HttpPut("{id}")]
+        public async Task<IActionResult> PutPessoa(int id, Pessoa pessoa)
+        {
+            if (id != pessoa.Id)
+            {
+                return BadRequest();
+            }
+
+            _context.Entry(pessoa).State = EntityState.Modified;
+
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!PessoaExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return NoContent();
+        }
+
+        [HttpPost]
+        public async Task<ActionResult<Pessoa>> PostPessoa(Pessoa pessoa)
+        {
+            _context.Pessoa.Add(pessoa);
+            await _context.SaveChangesAsync();
+
+            return CreatedAtAction("GetPessoa", new { id = pessoa.Id }, pessoa);
+        }
+
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeletePessoa(int id)
+        {
+            var pessoa = await _context.Pessoa.FindAsync(id);
+            if (pessoa == null)
+            {
+                return NotFound();
+            }
+
+            _context.Pessoa.Remove(pessoa);
+            await _context.SaveChangesAsync();
+
+            return NoContent();
+        }
+
+        private bool PessoaExists(int id)
+        {
+            return _context.Pessoa.Any(e => e.Id == id);
+        }
+    }
+}
---
+++ WebApi.CadastroPessoa/Data/WebApiCadastroPessoaContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+using WebApi.CadastroPessoa.Models;
+
+namespace WebApi.CadastroPessoa.Data
+{
+    public class WebApiCadastroPessoaContext : DbContext
+    {
+        public WebApiCadastroPessoaContext (DbContextOptions<WebApiCadastroPessoaContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Pessoa> Pessoa { get; set; } = default!;
+    }
+}
